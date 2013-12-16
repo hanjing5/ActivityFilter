240a2fe41e16726c99e3d7f3eeb3ea4c0f8783c9
h <h@ubuntu.(none)>
2013-12-15 20:29:02 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ActivityFilter::Application.config.secret_key_base = '0e57f23dc5bf59a33a61269383d14b5771288a863805e1b9a1d4ea11dbfab0d818800a07e2af98dac2d6ff58fbe796f9438cc8a57a8872560acf5b19cd4ca90b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ActivityFilter::Application.config.session_store :cookie_store, key: '_ActivityFilter_session'
